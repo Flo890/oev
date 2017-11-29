3f745b1fef9f34a3959da8a0df7f29fb64d9d227
Flo890 <florian.bemmann@gmx.de>
2017-11-29 11:27:52 +0100

    created build task: gradle clean fatJar and adapted gradle.properties file access

--- src/main/java/oev/App.java
+++ src/main/java/oev/App.java
@@ -2,6 +2,7 @@ package oev;
 
 import oev.mvc.Controller;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,13 +12,19 @@ public class App {
 
   public static void main(String[] args) {
     Properties prop = new Properties();
-    try {
-      InputStream input = new FileInputStream("gradle.properties");
-      prop.load(input);
 
-      new Controller(prop);
-    } catch (IOException e) {
-      throw new RuntimeException("could not find gradle.properties");
+    try {
+      // version 1: read file from jar root
+      prop.load(App.class.getResourceAsStream("/gradle.properties"));
+    } catch (Exception e) {
+      // if 1 does not work, try 2: read from project root
+      try {
+        prop.load(new FileInputStream("gradle.properties"));
+      } catch(Exception e2) {
+        throw new RuntimeException("could not find gradle.properties",e2);
+      }
     }
+
+    new Controller(prop);
   }
 }
